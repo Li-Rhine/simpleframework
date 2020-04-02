996501dda195a7ea150cfac4916961a1ab104531
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2020-04-03 02:07:30 +0800

    简易Spring框架搭建

--- src/main/java/com/imooc/HelloServlet.java
+++ src/main/java/com/imooc/HelloServlet.java
@@ -1,5 +1,8 @@
 package com.imooc;
 
+
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
+//    Logger log = LoggerFactory.getLogger(HelloServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = "我的简易框架";
+        log("name is "+ name);
         req.setAttribute("name", name);
         req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
